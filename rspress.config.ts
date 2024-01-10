561723a93e391b948dc129a809ecaf2f971aa480
Amadeus Mader <amadeus@mozart409.com>
2024-01-10 23:53:37 +0100

    update paths and added analytics

--- rspress.config.ts
+++ rspress.config.ts
@@ -27,6 +27,21 @@ export default defineConfig({
       }
     ]
   },
+  builderConfig: {
+    html: {
+      tags: [
+        // Configure Umami
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://umami-production-7f3d.up.railway.app/script.js',
+            'data-website-id': '545636d4-85f2-4b0f-8160-8a3a2f8044db'
+          }
+        }
+      ]
+    }
+  },
   markdown: {
     showLineNumbers: true
   },
